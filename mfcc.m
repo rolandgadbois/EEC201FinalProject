9dd0e73821b9393dee78fd49434e167b569e7833
rolandgadbois <roland@nochet.org>
2025-02-28 16:23:55 -0800

    Update mfcc.m

--- mfcc.m
+++ mfcc.m
@@ -1,27 +1,32 @@
 [x,fs]=audioread('Test_Data\s1.wav');
 sound(x);
 
-fs = 12500
+%fs = 12500
 
 % 20.48 milliseconds, contained in a block of 256 samples.
 
-time_axis = (0:length(x)-1) / fs;
-plot(time_axis, x);
-xlabel('Time (seconds)');
-ylabel('Amplitude');
-title('Signal in Time Domain');
-time_axis = (0:length(x)-1) / fs;
-plot(time_axis, x);
+time = (0:length(x)-1) / fs;
+plot(time, x);
 xlabel('Time (seconds)');
 ylabel('Amplitude');
 title('Time Spectra');
-xlim([0, 1])
 
+N = 128;
+M = round(N/3);
 
-[S, F, T] = spectrogram(x, 128, 128-43, 43, fs);
-imagesc(T * 1000, F, abs(S));
+[s, f, t] = spectrogram(x, hamming(128, 'periodic'), N - M, N, fs);
+imagesc(t * 1000, f, abs(s));
 axis xy;
 xlabel('Time (ms)');
 ylabel('Frequency (Hz)');
+colorbar;
 
-mel_filter_bank = melfb_own(20, 128, fs);
+m = melfb_own(20, N, fs);
+
+plot(linspace(0, (fs/2), N/2 + 1), melfb_own(20, N, fs)'),
+title('Mel-spaced filterbank'), xlabel('Frequency (Hz)');
+
+mel_spectrum = m * abs(s);
+mfcc = dct(log10(mel_spectrum));
+imagesc(t, [1, 20], mfcc);
+axis xy;
