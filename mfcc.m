c12d7e2c1399ebcc22fe13552cee6c2366493241
Adam Ashkenazi <adam100150@gmail.com>
2025-03-03 15:44:54 -0800

    Update mfcc.m to make it a functin that accepts the wave file name. It assumes that the directory "Test_Data" is in the working directory.

--- mfcc.m
+++ mfcc.m
@@ -1,32 +1,41 @@
-[x,fs]=audioread('Test_Data\s1.wav');
-sound(x);
+function mfcc(wav_file_name)
 
-%fs = 12500
+    full_path = strcat('Test_Data/',wav_file_name,'.wav');
 
-% 20.48 milliseconds, contained in a block of 256 samples.
 
-time = (0:length(x)-1) / fs;
-plot(time, x);
-xlabel('Time (seconds)');
-ylabel('Amplitude');
-title('Time Spectra');
+    [x,fs]=audioread(full_path);
+    sound(x);
 
-N = 128;
-M = round(N/3);
+    %fs = 12500
 
-[s, f, t] = spectrogram(x, hamming(128, 'periodic'), N - M, N, fs);
-imagesc(t * 1000, f, abs(s));
-axis xy;
-xlabel('Time (ms)');
-ylabel('Frequency (Hz)');
-colorbar;
+    % 20.48 milliseconds, contained in a block of 256 samples.
 
-m = melfb_own(20, N, fs);
+    time = (0:length(x)-1) / fs;
+    plot(time, x);
+    xlabel('Time (seconds)');
+    ylabel('Amplitude');
+    title('Time Spectra');
 
-plot(linspace(0, (fs/2), N/2 + 1), melfb_own(20, N, fs)'),
-title('Mel-spaced filterbank'), xlabel('Frequency (Hz)');
+    N = 128;
+    M = round(N/3);
 
-mel_spectrum = m * abs(s).^2;
-mfcc = dct(log10(mel_spectrum));
-imagesc(t, [1, 20], mfcc);
-axis xy;
+    [s, f, t] = spectrogram(x, hamming(128, 'periodic'), N - M, N, fs);
+    imagesc(t * 1000, f, abs(s));
+    axis xy;
+    xlabel('Time (ms)');
+    ylabel('Frequency (Hz)');
+    colorbar;
+
+    m = melfb_own(20, N, fs);
+
+    plot(linspace(0, (fs/2), N/2 + 1), melfb_own(20, N, fs)'),
+    title('Mel-spaced filterbank'), xlabel('Frequency (Hz)');
+
+    mel_spectrum = m * abs(s).^2;
+    mfcc_vals = dct(log10(mel_spectrum)); % shouldn't this be 
+    imagesc(t, [1, 20], mfcc_vals);
+    axis xy;
+    xlabel('Time (ms)');
+    ylabel('mfcc');
+    colorbar;
+end
