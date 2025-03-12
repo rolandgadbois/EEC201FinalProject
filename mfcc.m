b000ee4d10152514fbf9e61a6d52c47285b0d4e4
rolandgadbois <roland@nochet.org>
2025-03-12 11:57:59 -0700

    Add files via upload

--- mfcc.m
+++ mfcc.m
@@ -3,5 +3,5 @@ M = round(N/3);
 [s, f, t] = spectrogram(x, hamming(128, 'periodic'), N - M, N, fs);
 m = melfb_own(20, N, fs);
 mel_spectrum = m * abs(s).^2;
-mfcc = dct(log10(mel_spectrum));
+mfcc = dct(log10(max(mel_spectrum,1e-10)));
 end
